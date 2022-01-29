c60f2687cc628ad780447d3de2079c37304fbe2d
Nathan713 <49817256+Nathan713@users.noreply.github.com>
2022-01-28 21:18:45 -0500

    Update Register.php

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,43 +1,55 @@
-
 <?php
+  $inData = getRequestInfo();
 
-	$inData = getRequestInfo();
-
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "CRUD");
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName,Login,Password) VALUES (?,?,?,?)");
-		$stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
-		$stmt->execute();
-
-		$stmt->close();
-		$conn->close();
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-  function returnWithInfo( $firstName, $lastName, $id )
+  $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "CRUD");
+  if( $conn->connect_error )
   {
-    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    returnWithError( $conn->connect_error );
+  }
+  else
+  {
+    $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName,Login,Password) VALUES (?,?,?,?)");
+    $stmt->bind_param("ssss", $inData["firstname"], $inData["lastname"], $inData["login"], $inData["password"]);
+
+    if( $stmt->execute() )
+    {
+      $ret = "Account Created";
+      returnWithInfo( "Account Created" );
+    }
+    else
+    {
+      returnWithError("Account Failed to Create");
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
+
+  function getRequestInfo()
+  {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function getRequestInfo()
+  {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function sendResultInfoAsJson( $obj )
+  {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function returnWithError( $err )
+  {
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+  }
+
+  function returnWithInfo(  $success )
+  {
+    $retValue = '{"Success":"' . $success . '"}';
     sendResultInfoAsJson( $retValue );
   }
 
