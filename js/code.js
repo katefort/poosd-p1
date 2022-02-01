0dbcd8b1d88b7bb6cdebe4410bd024dcd146d43d
rebeccacasii <70347921+rebeccacasii@users.noreply.github.com>
2022-02-01 17:09:18 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,124 +1,118 @@
 const urlBase = 'http://group26poosd.xyz/LAMPAPI';
 const extension = 'php';
 
-//const form = document.getElementById("form");
-//const firstname = document.getElementById("firstname");
-//const lastname = document.getElementById("lastname");
-//const login = document.getElementById("login");
-//const password = document.getElementById("password");
 
-const userId = 0;
-const firstName = "";
-const lastName = "";
-
+let userId = 0;
+let firstName = "";
+let lastName = "";
 
 //referenced from RickL
 function register()
 {
+                // Add trim later
+                firstName = document.getElementById("firstNameNew").value;
+                lastName  = document.getElementById("lastNameNew").value;
 
-		firstName = getElementById("firstNameNew").value.trim();
-		lastName  = getElementById("lastNameNew").value.trim();
+                let login = document.getElementById("loginNew");
+                let password = document.getElementById("passwordNew");
 
-		let login = document.getElementById("loginNew");
-		let password = document.getElementById("passwordNew");
+                document.getElementById("registerItem").innerHTML="";
 
-		document.getElementById("registerItem").innerHTML="";
+                let tmp  = {login: login, password: password, firstname: firstName, lastname: lastName};
+                let jsonPayload = JSON.stringify(tmp);
 
-		let tmp  = {login: login, password: password, firstname: firstName, lastname: lastName};
-		let jsonPayload = JSON.stringify(tmp);
+                let url = urlBase + '/Register.'+extension;
+                console.log(login);
+                console.log(firstName);
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", url, true);
+                //CHECK MAY CAUSE PROBLEMS
+                xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8")
 
-		let url = urlBase + '/Register.'+extension;
+                try
+                {
+                        xhr.onreadystatechange = function()
+                        {
+                                if (this.readyState == 4 && this.status == 200)
+                                {
+                                        document.getElementById("registerItem").innerHTML = "User Created";
 
-		let xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		//CHECK MAY CAUSE PROBLEMS
-		xhr.setRequestHeader("Content-type", "application/json"; charset=UTF-8);
-
-		try
-		{
-			xhr.onreadystatechange = function()
-			{
-				if (this.readyState == 4 && this.status == 200)
-				{
-					document.getElementById("registerItem").innerHTML = "User Created";
-
-					let jsonObject= JSON.parse( xhr.responseText);
-
-					userId=jsonObject.id;
-					saveCookie();
+                                        let jsonObject= JSON.parse( xhr.responseText);
+;
+                                        saveCookie();
 //check to see if that is valid
-					window.location.href = "home.html"
-				}
-			};
-			xhr.send(jsonPayload);
-		} catch (err)
-		{
-			document.getElementById("registerItem").innerHTML= err.message;
+                                        window.location.href = "index.html";
+                                        
+                                    }
+                            };
+                            xhr.send(jsonPayload);
+                    } catch (err)
+                    {
+                            document.getElementById("registerItem").innerHTML= err.message;
+    
+                    }
+                }
+                //referenced from RickL
 
-		}
-
-}
-//referenced from RickL
 function doLogin()
 {
 
-	let login = document.getElementById("login");
-	let password = document.getElementById("password");
+        let login = document.getElementById("login");
+        let password = document.getElementById("password");
 
-	document.getElementById("loginItem").innerHTML="";
+        document.getElementById("loginItem").innerHTML="";
 
-	let tmp  = {login: login, password: password};
-	let jsonPayload = JSON.stringify(tmp);
+        let tmp  = {login: login, password: password};
+        let jsonPayload = JSON.stringify(tmp);
+        let url = urlBase + '/Login.'+extension;
 
-	let url = urlBase + '/Login.'+extension;
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        //CHECK MAY CAUSE PROBLEMS
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	//CHECK MAY CAUSE PROBLEMS
-	xhr.setRequestHeader("Content-type", "application/json"; charset=UTF-8);
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+                        if (this.readyState == 4 && this.status == 200)
+                        {
+                                let jsonObject = JSON.parse(xhr.responseText);
+                                userId = jsonObject.id;
 
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-			if (this.readyState == 4 && this.status == 200)
-			{
-				let jsonObject = JSON.parse(xhr.responseText)
-				userId = jsonObject.id;
+                                if(userId < 1)
+                                {
+                                        document.getElementById("loginItem").innerHTML = "User or Password is incorrect";
+                                        return;
+                                }
 
-				if(userId < 1)
-				{
-					document.getElementById("loginItem").innerHTML = "User or Password is incorrect"
-					return;
-				}
-
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
-				saveCookie();
+                                firstName = jsonObject.firstName;
+                                lastName = jsonObject.lastName;
+                                saveCookie();
 //check to see if that is valid
-				window.location.href = "home.html"
-			}
-		};
+                                window.location.href = "home.html";
+                        }
+                    };
 
-		xhr.send(jsonPayload);
+                    xhr.send(jsonPayload);
+    
+            } catch (err) {
+                    document.getElementById("loginItem").innerHTML= err.message;
+    
+            }
+    
 
-	} catch (err) {
-		document.getElementById("loginItem").innerHTML= err.message;
+    }
 
-	}
-
-}
-
-function saveCookie()
+    function saveCookie()
 {
 
-	let minutes = 20;
-	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000))
-	document.cookie = "firstName= " + firstName + ",lastName = " + lastName + ", userId = "+userId +" , expires = "+date.toGMTString();
+        let minutes = 20;
+        let date = new Date();
+        date.setTime(date.getTime()+(minutes*60*1000));
+        document.cookie = "firstName= " + firstName + ",lastName = " + lastName + ", userId = "+userId +" , expires = "+date.toGMTString();
 }
-
-/*
+    /*
 
 const button = document.getElementById("formType")
 let endpoint = "";
@@ -133,58 +127,59 @@ const password= document.getElementById("password");
 
 function user{
 
-	var username, password;
+        var username, password;
 
 function doLogin()
 {
-	userId = 0;
-	firstName = "";
-	lastName = "";
+        userId = 0;
+        firstName = "";
+        lastName = "";
 
-	let login = document.getElementById("loginName").value;
-	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
+        let login = document.getElementById("loginName").value;
+        let password = document.getElementById("loginPassword").value;
+//      var hash = md5( password );
 
-	document.getElementById("loginResult").innerHTML = "";
+        document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
+        let tmp = {login:login,password:password};
+//      var tmp = {login:login,password:hash};
+        let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/Login.' + extension;
+        let url = urlBase + '/Login.' + extension;
 
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function()
-		{
-			if (this.readyState == 4 && this.status == 200)
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+        try
+        {
+                xhr.onreadystatechange = function()
+                {
+                        if (this.readyState == 4 && this.status == 200)
+                        {
+                                let jsonObject = JSON.parse( xhr.responseText );
+                                userId = jsonObject.id;
 
-				if( userId < 1 )
-				{
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-					return;
-				}
+                                if( userId < 1 )
+                                {
+                          {
+                                        document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                                        return;
+                                }
 
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
+                                firstName = jsonObject.firstName;
+                                lastName = jsonObject.lastName;
 
-				saveCookie();
+                                saveCookie();
 
-				window.location.href = "color.html";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+                                window.location.href = "color.html";
+                        }
+                };
+                xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+                document.getElementById("loginResult").innerHTML = err.message;
+        }
 
 }
 
@@ -193,3 +188,4 @@ var publicAPI = {
     };
     return publicAPI;
 }*/
+
