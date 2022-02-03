fc2b89d999ebc8523a586819a1b55a88a5d52176
rebeccacasii <70347921+rebeccacasii@users.noreply.github.com>
2022-02-03 00:27:04 -0500

    added showAndHide()

--- js/code.js
+++ js/code.js
@@ -161,3 +161,16 @@ function searchContacts()
 	}
 	
 }
+
+function showAndHide() 
+{
+    var x = document.getElementById('show');
+
+	if (x.style.display == 'none') 
+	{
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+
+}
