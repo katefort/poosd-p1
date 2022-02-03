d5349cafd8b9ecbc9ce3f569adcc058d642fed64
jakiso <65683884+jakiso@users.noreply.github.com>
2022-02-02 22:35:32 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -117,7 +117,7 @@ function logout()
 	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 	window.location.href = "index.html";
 }
-function SearchContacts()
+function searchContacts()
 {
 	let srch = document.getElementById("searchText").value;
 	document.getElementById("contactSearchResult").innerHTML = "";
